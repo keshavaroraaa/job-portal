767a3a762067f4a297a81900e647f3cbbc1c693a
keshavaroraaa <arorakeshav762@gmail.com>
2026-05-20 00:02:09 +0530

    Delete client/src/api directory

--- client/src/api/applications.js
+++
@@ -1,9 +0,0 @@
-import API from './axios';
-
-export const applyForJob = (jobId, data) => API.post(`/applications/${jobId}/apply`, data, {
-  headers: { 'Content-Type': 'multipart/form-data' },
-});
-export const getMyApplications = (params) => API.get('/applications/my-applications', { params });
-export const getJobApplications = (jobId, params) => API.get(`/applications/${jobId}`, { params });
-export const updateApplicationStatus = (id, data) => API.put(`/applications/${id}/status`, data);
-export const getAllEmployerApplications = (params) => API.get('/applications/employer-all', { params });
